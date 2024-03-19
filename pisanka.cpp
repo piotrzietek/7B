0957cd8c45847bd9f5d6b2b48fc5d34325021e73
piotrzietek <piotr.zietek@gmail.com>
2024-03-19 18:14:33 +0100

    Create pisanka.cpp

---
+++ pisanka.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i < 20; i++) {
+        cout<<"         *  "<<endl;
+        cout<<"        *#*  "<<endl;
+        cout<<"       **#** "<<endl;
+        cout<<"      *#*#*#*"<<endl;
+        cout<<"       **#** "<<endl;
+        cout<<"        *#*  "<<endl;
+        cout<<"         *   "<<endl;
+
+        Sleep(200);
+        system("cls");
+
+        cout<<"         *  "<<endl;
+        cout<<"        *&*  "<<endl;
+        cout<<"       **&** "<<endl;
+        cout<<"      *&*&*&*"<<endl;
+        cout<<"       **&** "<<endl;
+        cout<<"        *&*  "<<endl;
+        cout<<"         *   "<<endl;
+        Sleep(200);
+        system("cls");
+    }
+    return 0;
+}
